8a02be87539a782351149f636f68994e11d8624e
rae1 <raelneninger@gmail.com>
2015-05-10 23:30:35 -0400

    Add support for Angularjs backing

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 var app = angular.module('html5boston', []);
 
-app.controller('MainController', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-}]);
+app.controller('mainController', [
+  '$scope',
+  function($scope) {
+    $scope.posts = [
+      { title: 'html5boston Group Meetup schedule page', link: 'http://www.meetup.com/html5boston/', upvotes: 234 },
+    ];
+
+   $scope.addPost = function() {
+      var title = $scope.title
+      var link = $scope.link
+
+      if (title && title === '') {
+        return;
+      }
+
+      $scope.posts.push({ title: title, link: link, upvotes: 0 });
+
+      $scope.title = '';
+      $scope.link = '';
+    }
+
+    $scope.upvote = function(post) {
+      post.upvotes += 1;
+    }
+  }
+]);
